0d71d28da2fed6dcc67fb6325c353c97aa236983
David <david@roxipr.in>
2023-06-15 10:33:18 -0700

    Update parameter.js for consistency

--- core/parameter/parameter.js
+++ core/parameter/parameter.js
@@ -1,9 +1,9 @@
 module.exports = class Parameter {
 
-    validationFunction
-    mappingFunction
-    required = false
-    name
+    validationFunction;
+    mappingFunction;
+    required = false;
+    name;
 
     /**
      * Sets the validation function
