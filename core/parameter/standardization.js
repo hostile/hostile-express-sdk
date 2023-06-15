57e3aa3013477c43b39bc083f2fd2e7d9083f57e
David <david@roxipr.in>
2023-06-15 10:33:49 -0700

    Update standardization.js for consistency

--- core/parameter/standardization.js
+++ core/parameter/standardization.js
@@ -1,27 +1,27 @@
 module.exports = {
-
+    
     standardizeEmail: (email) => {
-        return email.match(/^[^\s@]+@[^\s@]+\.[^\s@]+$/)
+        return email.match(/^[^\s@]+@[^\s@]+\.[^\s@]+$/);
     },
 
     standardizeUsername: (username) => {
-        return username.match(/^[a-zA-Z0-9]+$/)
+        return username.match(/^[a-zA-Z0-9]+$/);
     },
 
     // country code + number
     standardizePhone: (phone) => {
-        return phone.match(/^\+[1-9]\d{1,14}$/)
+        return phone.match(/^\+[1-9]\d{1,14}$/);
     },
 
     standardizeIP: (ip) => {
-        return ip.match(/^([0-9]{1,3}\.){3}[0-9]{1,3}$/)
+        return ip.match(/^([0-9]{1,3}\.){3}[0-9]{1,3}$/);
     },
 
     standardizeName: (name) => {
-        return name.match(/^[a-zA-Z]+ [a-zA-Z]+$/)
+        return name.match(/^[a-zA-Z]+ [a-zA-Z]+$/);
     },
 
     standardizeDomain: (domain) => {
-        domain.match(/^[a-zA-Z0-9]+(\.[a-zA-Z0-9]+)+$/)
+        domain.match(/^[a-zA-Z0-9]+(\.[a-zA-Z0-9]+)+$/);
     }
 }
